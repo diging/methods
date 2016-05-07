27165a78e7950808943fb2a505a8d7e78e812271
Erick Peirson <erick.peirson@asu.edu>
2016-05-07 13:45:06 -0400

    more reorganization, cleanup

---
+++ 1. Working with Texts/1.2 Change over time/helpers.py
@@ -0,0 +1,35 @@
+import nltk
+from nltk.corpus import stopwords
+
+wordnet = nltk.WordNetLemmatizer()
+stoplist = stopwords.words('english')
+
+
+def normalize_token(token):
+    """
+    Convert token to lowercase, and stem using the Porter algorithm.
+
+    Parameters
+    ----------
+    token : str
+
+    Returns
+    -------
+    token : str
+    """
+    return wordnet.lemmatize(token.lower())
+
+def filter_token(token):
+    """
+    Evaluate whether or not to retain ``token``.
+
+    Parameters
+    ----------
+    token : str
+
+    Returns
+    -------
+    keep : bool
+    """
+    token = token.lower()
+    return token not in stoplist and token.isalpha() and len(token) > 2
